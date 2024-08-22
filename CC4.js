2d04ea871d6c8039500251442a2070be85312331
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-08-22 11:33:54 +0700

    done part 1 Codechallenge 4

---
+++ CC4.js
@@ -0,0 +1,13 @@
+// Steven muốn xây dựng một công cụ tính tiền boa rất đơn giản mỗi khi anh ấy đi ăn ở nhà hàng. 
+// Ở đất nước của anh ấy, người ta thường boa 15% nếu giá trị hóa đơn nằm trong khoảng từ 50 đến 300.
+// Nếu giá trị khác, tiền boa là 20%.
+
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+// this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+// start with an if/else statement, and then try to convert it to a ternary
+// operator!)
+
+var billValue=300;
+var tips= billValue>=50 && billValue<=300? billValue*0.15: billValue*0.2;
+console.log(tips);  
+

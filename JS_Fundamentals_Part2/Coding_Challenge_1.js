588450097015b35f12a31f1ad5605d0e1147c320
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-08-27 23:03:07 +0700

    done JSF_p2 code_challenge_1

---
+++ JS_Fundamentals_Part2/Coding_Challenge_1.js
@@ -0,0 +1,24 @@
+var dolphins_score= [85,54,41];
+var koalas_score= [23,34,27];
+//1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+var calcAverage = (points) =>{
+    var sum = points.reduce((accumulator,currentValue)=>{
+        return accumulator += currentValue;
+    },0);
+    return (sum/3).toFixed(2);
+};
+//2. Use the function to calculate the average for both teams
+var avgDolphins= calcAverage(dolphins_score);
+console.log(avgDolphins);
+
+var avgKoalas= calcAverage(koalas_score);
+console.log(avgKoalas);
+
+//3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+//to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+
+function checkWinner(avgDolphin, avgKoala){
+    return avgDolphin >= avgKoala ? `Dolphins win (${avgDolphin} vs. ${avgKoala})`:`Koalas win (${avgKoala} vs. ${avgDolphin})`;
+}
+
+console.log(checkWinner(avgDolphins,avgKoalas)); 

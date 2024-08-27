35a033a9ee7508972e4d73a0d5b6ed75049adcfd
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-08-27 22:24:59 +0700

    done code challenge 4

--- CC4.js
+++ CC4.js
@@ -7,7 +7,13 @@
 // start with an if/else statement, and then try to convert it to a ternary
 // operator!)
 
-var billValue=300;
+var billValue=275;
 var tips= billValue>=50 && billValue<=300? billValue*0.15: billValue*0.2;
 console.log(tips);  
 
+// 2. Print a string to the console containing the bill value, the tip, and the final value
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+// 316.25” 
+
+var result = `The bill was ${billValue}, the tip was ${tips}, and the total value ${billValue+tips}`;
+console.log(result);
